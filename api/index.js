7591322687bb26334627578ae46e2d715cc21ff9
Ashish Sharma <ashish94065@gmail.com>
2024-02-06 18:04:17 +0530

    mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('DB connected successfully')
+})
+.catch((err) => {
+    console.error(err);
+})
+
 const app = express();
 
 app.listen(3000, () => {
