a788daa1cd81ea54086ae4803131b936816a4989
Ashish Sharma <ashish94065@gmail.com>
2024-02-07 10:50:04 +0530

    Error Handler middleware

--- api/index.js
+++ api/index.js
@@ -17,8 +17,20 @@ const app = express();
 
 app.use(express.json())
 
+//routes middleware
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
+//error handling middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000!")
